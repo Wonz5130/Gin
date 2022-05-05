cefc89784691a9addc58456b3f76b5be999174d5
wangzhou <wangzhou@guanghe.tv>
2022-05-05 10:54:07 +0800

    upload codes

---
+++ pkg/setting/setting.go
@@ -0,0 +1,21 @@
+package setting
+
+import "github.com/spf13/viper"
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting() (*Setting, error) {
+	vp := viper.New()
+	// viper 是允许设置多个配置路径
+	vp.SetConfigName("config")
+	vp.AddConfigPath("configs/")
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Setting{vp}, nil
+}
